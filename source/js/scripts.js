f62d2b95dd2065c945478b69f7911a1d16845571
dimanakone <dimka39@gmail.com>
2022-09-02 17:30:02 +0300

    правки по критериям Б5 Б17 Б27 Б30

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,7 +1,7 @@
-var navToggle = document.querySelector(".main-nav__toggle");
+var navToggle = document.querySelector(".page-header__toggle");
 var nav = document.querySelector(".main-nav");
 
-navToggle.classList.remove("main-nav__toggle--nojs");
+navToggle.classList.remove("page-header__toggle--nojs");
 nav.classList.remove("main-nav--nojs");
 
 navToggle.addEventListener("click", function () {
@@ -9,13 +9,13 @@ navToggle.addEventListener("click", function () {
     nav.classList.remove("main-nav--closed");
     nav.classList.add("main-nav--opened");
 
-    navToggle.classList.remove("main-nav__toggle--closed");
-    navToggle.classList.add("main-nav__toggle--opened");
+    navToggle.classList.remove("page-header__toggle--closed");
+    navToggle.classList.add("page-header__toggle--opened");
   } else {
     nav.classList.remove("main-nav--opened");
     nav.classList.add("main-nav--closed");
 
-    navToggle.classList.remove("main-nav__toggle--opened");
-    navToggle.classList.add("main-nav__toggle--closed");
+    navToggle.classList.remove("page-header__toggle--opened");
+    navToggle.classList.add("page-header__toggle--closed");
   }
 })
